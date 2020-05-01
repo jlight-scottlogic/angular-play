1dc5c1e8807bfea6a6215028e60fad5b1f497ded
Jim Light <jlight@scottlogic.com>
2020-05-01 11:36:06 +0100

    some styling

--- src/app/products/list/list.display.component.ts
+++ src/app/products/list/list.display.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
 import { Product } from '../models';
 import { ButtonComponent } from './button/button.component';
+import { AgGridAngular } from 'ag-grid-angular';
 
 @Component({
   selector: 'list-display',
@@ -8,6 +9,8 @@ import { ButtonComponent } from './button/button.component';
   styleUrls: ['./list.display.component.scss']
 })
 export class ListDisplayComponent {
+  @ViewChild('agGrid') agGrid: AgGridAngular;
+  
   @Input() public products: Product[] = [];
 
   public columnDefs = [
@@ -21,17 +24,26 @@ export class ListDisplayComponent {
       headerName: 'Description',
       field: 'description',
       sortable: true,
-      filter: true
+      filter: true,
+      flex: 1
     },
     {
       headerName: 'Date Added',
       field: 'dateAdded',
       sortable: true,
       filter: true,
+      width: 140,
       valueFormatter: ({ value }) => value.format('YYYY-MM-DD')
     },
     {
-      cellRendererFramework: ButtonComponent
+      cellRendererFramework: ButtonComponent,
+      cellClass: 'p-0',
+      width: 75
     }
   ];
+
+  onGridReady() {
+    // this.agGrid.columnApi.autoSizeAllColumns();
+    // this.agGrid.api.sizeColumnsToFit();
+  }
 }
