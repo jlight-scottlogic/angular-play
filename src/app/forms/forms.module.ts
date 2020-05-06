2c32d37ac7cfafdda54bfb5439f8f13bad1f9c32
Jim Light <jlight@scottlogic.com>
2020-05-06 09:02:49 +0100

    basic form

--- src/app/forms/forms.module.ts
+++ src/app/forms/forms.module.ts
@@ -1,19 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { FormsModule as NgFormsModule } from '@angular/forms';
-
-
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatMomentDateModule } from '@angular/material-moment-adapter';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    NgFormsModule,
-    MatFormFieldModule
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatMomentDateModule
   ],
   exports: [
-    NgFormsModule
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatMomentDateModule
+  ],
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: 'en-GB' }
   ]
 })
 export class FormsModule { }
--- src/app/products/create/create.display.component.ts
+++ src/app/products/create/create.display.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Product } from '../models';
 
 @Component({
   selector: 'create-display',
   templateUrl: './create.display.component.html',
   styleUrls: ['./create.display.component.scss']
 })
-export class CreateDisplayComponent implements OnInit {
+export class CreateDisplayComponent {
 
-  constructor() { }
+  @Output() onSubmit = new EventEmitter<Product>()
 
-  ngOnInit(): void {
+  form = new FormGroup({
+    name: new FormControl(),
+    description: new FormControl(),
+    dateAdded: new FormControl()
+  })
+
+  public onSubmitClick(event: MouseEvent) {
+    event.preventDefault();
+
+    this.onSubmit.emit({
+      id: null,
+      name: this.form.value.name,
+      description: this.form.value.description,
+      dateAdded: this.form.value.dateAdded,
+      isActive: true
+    });
   }
-
 }
